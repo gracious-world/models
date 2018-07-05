f0ed606c304e99bb5f3638319bf9b25417701029
mac <mac@AppledeMacBook-Pro.local>
2018-07-05 12:23:41 +0800

    fix huitian wxsk payment name

--- payment/PaymentHUITIANWXSK.php
+++ payment/PaymentHUITIANWXSK.php
@@ -6,7 +6,7 @@
  * Date: 18-1-26
  * Time: 下午4:47
  */
-class PaymentHUITIANWX extends PaymentHUITIANWY {
+class PaymentHUITIANWXSK extends PaymentHUITIANWY {
 
     protected $paymentName= 'huitianwx';
     public $qrDirName = 'huitianwx';
